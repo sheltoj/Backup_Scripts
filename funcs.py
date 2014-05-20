b9d8572c69ba9f04144ca49615621c34153a1194
Thomas Cate <thomascate@gmail.com>
2014-05-20 08:27:29 -0500

    indention fixes

--- funcs.py
+++ funcs.py
@@ -4,35 +4,35 @@ import time
 import hashlib
 
 def dir_list(path):
-	fileNames = []
-	start = time.time()
-	for root, dirs, files in os.walk(path):
-		if files:
-			for item in files:
-				fileNames.append(root + "\" + item)
-	end = time.time()
-	return fileNames
+  fileNames = []
+  start = time.time()
+  for root, dirs, files in os.walk(path):
+    if files:
+      for item in files:
+        fileNames.append(root + "\\" + item)
+  end = time.time()
+  return fileNames
 
 def file_md5(fileName)
-        fileHandle = open(fileName,'rb')
-        fileContents = fileHandle.read()
-        fileHash = hashlib.md5(open(fileName).read()).hexdigest()
-        return fileHash
+  fileHandle = open(fileName,'rb')
+  fileContents = fileHandle.read()
+  fileHash = hashlib.md5(open(fileName).read()).hexdigest()
+  return fileHash
 
 
 def get_attributes(fileName):
-	attributes = {}
-        attributes['path'] = fileName
-	attributes['name'] = os.path.basename(fileName)
-        attributes['size'] = os.path.getsize(fileName)
-	attributes['uuid'] = uuid.uuid4()
-        attributes['md5'] = hashlib.md5(open(fileNames[0],'rb').read()).hexdigest()
-	return attributes	
+  attributes = {}
+  attributes['path'] = fileName
+  attributes['name'] = os.path.basename(fileName)
+  attributes['size'] = os.path.getsize(fileName)
+  attributes['uuid'] = uuid.uuid4()
+  attributes['md5'] = hashlib.md5(open(fileNames[0],'rb').read()).hexdigest()
+  return attributes	
 
 
- for i in range(0, 100):
-	try:
-		test = get_attributes(fileNames[i])
-		print(test)
-	except:
-		print("error on " + fileNames[i])
+for i in range(0, 100):
+  try:
+    test = get_attributes(fileNames[i])
+  print(test)
+    except:
+  print("error on " + fileNames[i])
