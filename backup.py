cb0527d7bacbac560594fabe747db77e775518b2
Thomas Cate <thomascate@gmail.com>
2014-05-20 10:00:16 -0500

    check if dbfile exists and initialize if it doesn't

--- backup.py
+++ backup.py
@@ -7,6 +7,13 @@ fileNames = dir_list(arguments.path)
 for files in fileNames:
   try:
     test = get_attributes(files)
-    print(test)
+#    print(test)
   except Exception, e:
     print("error on " + files + " " + str(e))
+
+dbFile = ((os.path.split(os.path.realpath(__file__)))[0] + "/" + arguments.dbFile)
+
+if not os.path.isfile(dbFile):
+  init_database(dbFile)
+else:
+  print "dbfile found"
