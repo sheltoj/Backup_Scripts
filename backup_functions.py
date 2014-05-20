39acf38b884c5c09e61edefd66015a09f8a60fd7
Thomas Cate <thomascate@gmail.com>
2014-05-20 13:45:32 -0500

    added insert function and cleaned up init_database

--- backup_functions.py
+++ backup_functions.py
@@ -55,23 +55,27 @@ def init_database(dbfile):
   conn = sqlite3.connect(dbfile)
   c = conn.cursor()
   c.execute('''CREATE TABLE files (
-                                   "path" text,
-                                   "name" text,
-                                   "size" integer,
-                                   "md5" text,
-                                   "x-amz-sha256" text,
-                                   "modification_time" text,
-                                   "uploaded" integer,
-                                   "uploaded_at" text
+                                   "path" TEXT UNIQUE NOT NULL,
+                                   "name" TEXT,
+                                   "size" INTEGER,
+                                   "md5" TEXT,
+                                   "x-amz-sha256" TEXT,
+                                   "modification_time" TEXT,
+                                   "uploaded" TEXT,
+                                   "uploaded_at" TEXT
                                   )
             ''')
   conn.commit
   conn.close
 
+def insert_host(attributes,dbFile):
+  conn = sqlite3.connect(dbFile)
+  c = conn.cursor()
+  print (type(attributes['name']))
+  c.execute('''INSERT INTO files (path, name) VALUES (?,?)''', ( attributes['path'], attributes['name'] ) )
 
-
-
-
+  conn.commit()
+  conn.close()
 
 
 
