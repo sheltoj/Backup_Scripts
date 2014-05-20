64fe37ffe075e4561a39d0c7b5490a23c43b0db6
Thomas Cate <thomascate@gmail.com>
2014-05-20 09:36:45 -0500

    added init_db function

--- backup_functions.py
+++ backup_functions.py
@@ -5,6 +5,8 @@ import time
 import hashlib
 import argparse
 import errno
+import sqlite3
+import time
 
 def get_input():
   parser = argparse.ArgumentParser(description='Glacier backup and dupcheck script')
@@ -48,3 +50,34 @@ def get_attributes(fileName):
   attributes['uuid'] = uuid.uuid4()
   attributes['md5'] = hashlib.md5(open(fileName,'rb').read()).hexdigest()
   return attributes	
+
+def init_database(dbfile):
+  conn = sqlite3.connect(dbfile)
+  c = conn.cursor()
+  c.execute('''CREATE TABLE files (
+                                   "path" text,
+                                   "name" text,
+                                   "size" integer,
+                                   "md5" text,
+                                   "x-amz-sha256" text,
+                                   "modification_time" text,
+                                   "uploaded" integer,
+                                   "uploaded_at" text
+                                  )
+            ''')
+  conn.commit
+  conn.close
+
+
+
+
+
+
+
+
+
+
+
+
+
+
